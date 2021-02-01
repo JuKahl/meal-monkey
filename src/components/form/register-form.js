e01cfb047c7f6e16927d77cc03fabaec3041f90e
JuKahl <julian.kahl@hotmail.de>
2021-02-01 17:05:08 +0100

    Add password form

--- src/components/form/register-form.js
+++ src/components/form/register-form.js
@@ -1,10 +1,30 @@
 export function createRegisterForm() {
-    return `   
-    <form class="form">
-    <h2>Reset password</h2>
-    <p>Please enter your email to receive a link to create a new password via email.</p>
-    <input placeholder="email" />
-    <input type="submit"/>
-    </form>
-    `;
+//     return `   
+//     <form class="form">
+//     <h2>Reset password</h2>
+//     <p>Please enter your email to receive a link to create a new password via email.</p>
+//     <input placeholder="email" />
+//     <input type="submit"/>
+//     </form>
+//     `;
+// }
+const form = document.createElement ("form");
+form.className = "form";
+
+const title = document.createElement ("h2");
+title.innerText = "Reset password";
+title.className = "form"
+
+const button = document.createElement ("button");
+button.innerText = "Press here!";
+button.className = "btn";
+
+function register () {
+    alert("Registered");
+}
+button.addEventListener("click", register)
+
+form.append(title, button);
+
+return form;
 }
